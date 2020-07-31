fa7a9f4230c1acd00b1b1d267adabba9245ff24e
Rostyslav Druzhchenko <rdruzhchenko@teamworkcommerce.com>
2020-07-31 11:19:46 +0300

    Add tests

--- tests/tr00_pdf/ts00_errors/t01_2params.sh
+++ tests/tr00_pdf/ts00_errors/t01_2params.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+source ../../../lib/asserts.sh
+
+# Test 01-1 - Two params
+EXP="usage: ./pathfinder [filename]$"
+RES="$(./$1 a b 2>&1 | cat -e)"
+ASSERT_EQUAL "$EXP" "$RES"
+
+exit $TEST_FAILED
--- tests/tr00_pdf/ts00_errors/t02_file_does_not_exist.sh
+++ tests/tr00_pdf/ts00_errors/t02_file_does_not_exist.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+source ../../../lib/asserts.sh
+
+# Test 01-1 - Two params
+EXP="error: file [absent_file] does not exist$"
+RES="$(./$1 absent_file 2>&1 | cat -e)"
+ASSERT_EQUAL "$EXP" "$RES"
+
+exit $TEST_FAILED
